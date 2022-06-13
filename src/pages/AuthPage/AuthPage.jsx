fe15b7d1f8eb43627d8c14a66ad31ee04caf0556
dani-diaz <daniela.diaz0808@gmail.com>
2022-06-13 03:24:00 -0700

    authlogo

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -7,7 +7,7 @@ export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <body className="AuthPage">
-      <img className="dinologo" src="https://i.imgur.com/msn6BGP.png" alt="" />
+      <img className="authlogo" src="https://i.imgur.com/msn6BGP.png" alt="" />
       <div className="container-auth">
       <button className="auth-button" onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       <h1> </h1>
