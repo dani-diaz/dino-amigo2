2f1e3ead808b7aa9ead67b7cd429710af24061d0
dani-diaz <daniela.diaz0808@gmail.com>
2022-06-13 03:28:19 -0700

    authlogin button

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -33,7 +33,7 @@ export default function LoginForm({ setUser }) {
           <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
           <label>Password</label>
           <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
-          <button className="auth-button" type="submit">LOG IN</button>
+          <button className="auth-login-button" type="submit">LOG IN</button>
         </form>
         </div>
       </div>
