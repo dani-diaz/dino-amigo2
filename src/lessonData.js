c2a50b5d202286e1113f0ca87a7f9810704ff6b6
dani-diaz <daniela.diaz0808@gmail.com>
2022-06-13 05:11:38 -0700

    lessons video/pics updated

--- src/lessonData.js
+++ src/lessonData.js
@@ -1,30 +1,27 @@
 export const lessons = [
   {number: '📕  Lesson #1',
-    title: 'Los Colores', 
+    title: 'La Casa', 
   video: "https://www.youtube.com/embed/8yuiUvi568I?start=07&end=84",
-  lessonImage: "https://i.imgur.com/ptC4St1.png"
+  lessonImage: "https://i.imgur.com/VKImtsI.png"
   }, 
 
   {number: '📘 Lesson #2',
-    title: 'Los Animales', 
+    title: 'Las Cosas', 
   video: "https://www.youtube.com/embed/8yuiUvi568I?start=169&end=226",
-  lessonImage: "https://i.imgur.com/ptC4St1.png"	  
+  lessonImage: "https://i.imgur.com/9DBwJKS.png"	  
   }, 
 
-  {title: 'EL Closet', 
-  video: "https://www.youtube.com/embed/8yuiUvi568I?start=170&end=200",
-  lessonImage: "https://i.imgur.com/ptC4St1.png" 
+  {number: '📗 Lesson #3',
+  title: 'EL Cajon', 
+  video: "https://www.youtube.com/embed/8yuiUvi568I?start=229&end=351",
+  lessonImage: "https://i.imgur.com/TLlzlLT.png" 
   }, 
 
-  {title: 'La Casa',
-  video: "https://www.youtube.com/embed/8yuiUvi568I?start=170&end=200",
-  lessonImage: "https://i.imgur.com/ptC4St1.png"
+  {number: '📙 Lesson #4',
+  title: 'Los Numeros',
+  video: "https://www.youtube.com/embed/8yuiUvi568I?start=441&end=519",
+  lessonImage: "https://i.imgur.com/a6O0ZTJ.png"
   },
-
-  {title: 'La Familia', 
-  video: "https://www.youtube.com/embed/8yuiUvi568I?start=170&end=200",
-  lessonImage: "https://i.imgur.com/ptC4St1.png"	  
-  }, 
 
 ];
 	
